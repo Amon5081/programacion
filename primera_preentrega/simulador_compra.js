065ea26b057eb1d3cc1aceb0ccdf8265cff7632b
abrahan <abrahancorona15@hotmail.com>
2023-12-08 02:42:46 -0300

    preentrega

---
+++ primera_preentrega/simulador_compra.js
@@ -0,0 +1,18 @@
+//objetivo:crear un simulador de carrito que diga el precio final con añadidos como iva, envio
+
+//variables
+let producto = Number(prompt("precio del producto: "));
+let iva = 0.5;
+let envio = 2000;
+
+//condicional base
+
+if (producto) {
+    alert(producto * iva + envio);
+
+} else {
+    producto != Number
+    alert("no ingresaste un numero");
+}
+
+
